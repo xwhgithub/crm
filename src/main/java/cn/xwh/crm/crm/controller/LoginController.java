2fd91f90c52ed18d483e269e8db4348abb24b205
xwhgithub <1026064137@qq.com>
2019-10-07 16:41:49 +0800

    测试新键盘

--- src/main/java/cn/xwh/crm/crm/controller/LoginController.java
+++ src/main/java/cn/xwh/crm/crm/controller/LoginController.java
@@ -52,6 +52,7 @@ public class LoginController {
                 System.out.println("=====第二次");
                 System.out.println("=========第三次");
                 System.out.println("===组员");
+                System.out.println("试试新键盘,赶脚还可以");
 
             } catch ( AuthenticationException ae ) {
                 model.addAttribute("msg", ae.getMessage());// 登录失败移除用户
