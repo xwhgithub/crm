9c313d2e2c776f82a08ce15c79e2396dbb08df82
xwhgithub <1026064137@qq.com>
2019-10-07 15:40:08 +0800

    测试git提交,第三次增加syso

--- src/main/java/cn/xwh/crm/crm/controller/LoginController.java
+++ src/main/java/cn/xwh/crm/crm/controller/LoginController.java
@@ -50,6 +50,7 @@ public class LoginController {
                 currentUser.login(token);
                 System.out.println("===测试git");
                 System.out.println("=====第二次");
+                System.out.println("=========第三次");
 
             } catch ( AuthenticationException ae ) {
                 model.addAttribute("msg", ae.getMessage());// 登录失败移除用户
